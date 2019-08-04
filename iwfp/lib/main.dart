17da345e77166bd2dc8f15cbf8c1bea5c051b2f9
tianhaoz95 <tianhaoz@umich.edu>
2019-08-04 01:05:26 -0700

    hello world

--- iwfp/lib/main.dart
+++ iwfp/lib/main.dart
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Hello World!'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Wow this worked!',
             ),
             Text(
               '$_counter',
